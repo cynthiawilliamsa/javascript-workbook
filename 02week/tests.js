18453e0907fa42fdb6cb18c78f1c0bc4a0f7559d
cynthiawilliamsa <cynthiawilliamsa@gmail.com>
2018-07-29 16:34:07 -0500

    added required test cases and some extras

--- 02week/tests.js
+++ 02week/tests.js
@@ -1,38 +1,63 @@
-function rockPaperScissors(hand1, hand2) { 
-    const cleanHand1 = hand1.toLowerCase().trim(); //re-assign the input value to a variable if scrubbing. 
-    const cleanHand2 = hand2.toLowerCase().trim();  
-    if(!Number(cleanHand1) && !Number(cleanHand2) || cleanHand1 === 'rock' || cleanHand1 === 'paper' || cleanHand1 === 'scissors' && cleanHand2 === 'rock' || cleanHand2 === 'paper' || cleanHand2 === 'scissors'){
-      if (cleanHand1 === cleanHand2) {
-        return "It's a tie!";
-      } else if (cleanHand1 === 'paper' && cleanHand2 === 'rock' || 
-                cleanHand1 === 'rock' && cleanHand2 === 'scissors' || 
-                cleanHand1 === 'scissors' && cleanHand2 === 'paper') 
-                {
-        return "Hand one wins!";
-      } else if (cleanHand2 === 'paper' && cleanHand1 === 'rock' || 
-                cleanHand2 === 'scissors' && cleanHand1 === 'paper' || 
-                cleanHand2 === 'rock' && cleanHand1 === 'scissors')
-                {
-        return 'Hand two wins!';
-        } 
-    }  else {
-      return "please enter rock, paper or scissors";
-    } 
+const assert = require("assert");
+
+function rockPaperScissors(hand1, hand2) {
+  const cleanHand1 = hand1.toLowerCase().trim(); //re-assign the input value to a variable if scrubbing.
+  const cleanHand2 = hand2.toLowerCase().trim();
+ 
+  if (
+    (!Number(cleanHand1) && !Number(cleanHand2)) &&
+    (cleanHand1 === "rock" || cleanHand1 === "paper" || cleanHand1 === "scissors") &&
+    (cleanHand2 === "rock" || cleanHand2 === "paper" || cleanHand2 === "scissors")
+  ) {
+
+
+    if (cleanHand1 === cleanHand2) {
+      return "It's a tie!";
+    } else if (
+      (cleanHand1 === "paper" && cleanHand2 === "rock") ||
+      (cleanHand1 === "rock" && cleanHand2 === "scissors") ||
+      (cleanHand1 === "scissors" && cleanHand2 === "paper")
+    ) {
+      return "Hand one wins!";
+    } else if (
+      (cleanHand2 === "paper" && cleanHand1 === "rock") ||
+      (cleanHand2 === "scissors" && cleanHand1 === "paper") ||
+      (cleanHand2 === "rock" && cleanHand1 === "scissors")
+    ) {
+      return "Hand two wins!";
+    } else {
+      return ":("
+    }
+  } else {
+
+    return "please enter rock, paper or scissors";
   }
-  //test for all possible Hand 1 wins
-  //test for all possible hand 2 wins
-  //test for valid input
-  if (typeof describe === 'function') {
-    describe('#rockPaperScissors()', () => {     
-      it('should detect which hand one win', () => {
-        assert.equal(rockPaperScissors('paper', 'rock'), "Hand one wins!");
-        assert.equal(rockPaperScissors('scissors', 'paper'), "Hand one wins!");
-        assert.equal(rockPaperScissors('rock', 'scissors'), "Hand one wins!");
-      });
-      it('should scrub input to ensure lowercase with "trim"ed whitepace', () => {
-        assert.equal(rockPaperScissors('rOcK', ' paper '), "Hand two wins!");
-        assert.equal(rockPaperScissors('Paper', 'SCISSORS'), "Hand two wins!");
-        assert.equal(rockPaperScissors('rock ', 'sCiSsOrs'), "Hand one wins!");
-      });
+}
+//test for all possible Hand 1 wins
+//test for all possible hand 2 wins
+//test for valid input
+if (typeof describe === "function") {
+  describe("#rockPaperScissors()", () => {
+    it('should detect which hand one win', () => {
+      assert.equal(rockPaperScissors('paper', 'rock'), "Hand one wins!");
+      assert.equal(rockPaperScissors('scissors', 'paper'), "Hand one wins!");
+      assert.equal(rockPaperScissors('rock', 'scissors'), "Hand one wins!");
     });
-  }
+    it('should detect which hand two win', () => {
+      assert.equal(rockPaperScissors('rock', 'paper'), "Hand two wins!");
+      assert.equal(rockPaperScissors('paper', 'scissors'), "Hand two wins!");
+      assert.equal(rockPaperScissors('scissors', 'rock'), "Hand two wins!");
+    });
+    it('should scrub input to ensure lowercase with "trim"ed whitepace', () => {
+      assert.equal(rockPaperScissors('rOcK', ' paper '), "Hand two wins!");
+      assert.equal(rockPaperScissors('Paper', 'SCISSORS'), "Hand two wins!");
+      assert.equal(rockPaperScissors('rock ', 'sCiSsOrs'), "Hand one wins!");
+    });
+    it("valid rock, paper, scissors", () => {
+      //what's wrong here?
+      assert.equal(rockPaperScissors("rock", " "), "please enter rock, paper or scissors");
+      assert.equal(rockPaperScissors("Paper", "smil8"), "please enter rock, paper or scissors");
+      assert.equal(rockPaperScissors("rocket ", "sCiSsOrs"), "please enter rock, paper or scissors");
+    });
+  });
+}
